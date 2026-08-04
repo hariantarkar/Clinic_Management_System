c7ee51e7244bfb4752214573279b44935409c9b7
hari antarkar <harishantarkar3738@gmail.com>
2026-08-04 16:31:48 +0530

    temp admin created

--- CMSBackend/src/main/java/com/CMS/RegisterController/RegisterController.java
+++ CMSBackend/src/main/java/com/CMS/RegisterController/RegisterController.java
@@ -29,7 +29,12 @@ public class RegisterController {
 	 * 
 	 * }
 	 */
-	
+	@PostMapping("/auth/create-first-admin")
+	public ResponseEntity<String> createFirstAdmin(@Valid @RequestBody Register register) {
+	    register.setUserType(Register.UserType.admin);
+	    String msg = authService.register(register);
+	    return new ResponseEntity<>(msg, HttpStatus.CREATED);
+	}
 	@PostMapping("/auth/reg")
 	public ResponseEntity<String> registerUser(@Valid @RequestBody Register register) {
 
